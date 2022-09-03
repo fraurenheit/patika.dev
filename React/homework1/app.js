f79775b4aae252c395b2341aa8ed1679c3649c12
fraurenheit <iremmbuyukcingil@gmail.com>
2022-09-03 20:25:21 +0300

    last commit.

--- React/homework1/app.js
+++ React/homework1/app.js
@@ -1,19 +1,5 @@
 import axios from "axios";
 
-// export default async (user_id) => {
-//     try {
-//         const{ data:user } = await axios(
-//             "https://jsonplaceholder.typicode.com/users/1" + user_id);
-
-//         const{ data:posts } = await axios(
-//             "https://jsonplaceholder.typicode.com/posts?userId=1" + user_id);
-            
-//         return { user, posts };
-//     } catch (e) {
-//         return e;
-//     }
-// };
-
 const GetData = async (user_id) => {
 
     const { data: user }= await axios("https://jsonplaceholder.typicode.com/users/" + user_id);
